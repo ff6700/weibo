5b4c83c0da55ad3c4856068dc77d2adb161047c5
vagrant <vagrant@homestead>
2020-09-20 19:23:19 +0800

    用户微博列表

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Status::class, function (Faker $faker) {
+    $date_time = $faker->date .'  '.$faker->time;
+    return [
+        //
+        'user_id' => $faker->randomElement(['1','2','3','4']),
+        'content'=>$faker->text(),
+        'created_at'=>$date_time,
+        'updated_at'=>$date_time,
+    ];
+});
